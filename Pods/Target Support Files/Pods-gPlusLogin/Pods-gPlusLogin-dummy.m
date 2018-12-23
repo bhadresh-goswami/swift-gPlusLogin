eec4b465b560c84e9f578c3ac3f77cc219be76b4
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-22 20:36:34 -0800

    done

---
+++ Pods/Target Support Files/Pods-gPlusLogin/Pods-gPlusLogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gPlusLogin : NSObject
+@end
+@implementation PodsDummy_Pods_gPlusLogin
+@end
--- gPlusLogin/ViewController.swift
+++ gPlusLogin/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,GIDSignInUIDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+    }
+    
+    @IBOutlet weak var buttongoogleSignIn: GIDSignInButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance().uiDelegate = self as! GIDSignInUIDelegate
+
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    func signInWillDispatch(signIn: GIDSignIn!, error: NSError!) {
+        //myActivityIndicator.stopAnimating()
+    }
+    
+    // Present a view that prompts the user to sign in with Google
+    func signIn(signIn: GIDSignIn!,
+                presentViewController viewController: UIViewController!) {
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapSignOut(_ sender: AnyObject) {
+        GIDSignIn.sharedInstance().signOut()
     }
 
     override func didReceiveMemoryWarning() {
